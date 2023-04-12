ba9235447b931a8497b87326479d7cb053fe72eb
peachboii <my.janelle.tan@gmail.com>
2023-04-12 15:26:59 +0800

    First commit

---
+++ wordle.py
@@ -0,0 +1,4 @@
+def difficulty():
+    user_input = input("Please select your game difficulty...\n1. Easy\n    -5 letter word\n    -infinite tries\n\n2. Default\n    -5 letter word\n    -5 tries\n\n3. Hard\n    -7+ letter word\n    -5 tries\n\n4. \n    -5 letter word\n    -infinite tries")
+
+difficulty()
