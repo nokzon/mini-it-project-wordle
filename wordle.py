43dae1a6c73627f846968aa7d0d041a62f0b379c
peachboii <my.janelle.tan@gmail.com>
2023-04-12 15:33:37 +0800

    added features

--- wordle.py
+++ wordle.py
@@ -1,4 +1,5 @@
 def difficulty():
-    user_input = input("Please select your game difficulty...\n1. Easy\n    -5 letter word\n    -infinite tries\n\n2. Default\n    -5 letter word\n    -5 tries\n\n3. Hard\n    -7+ letter word\n    -5 tries\n\n4. \n    -5 letter word\n    -infinite tries")
+    user_input = input("Please select your game difficulty....\n1. Easy\n    -5 letter word\n    -infinite tries\n\n2. Default\n    -5 letter word\n    -5 tries\n\n3. Hard\n    -7+ letter word\n    -5 tries\n\n4. \n    -5 letter word\n    -infinite tries")
 
 difficulty()
+print("you can play wordle to improve your critical thinking")
